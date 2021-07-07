512230c6510ec9c79645400ed0c200c11832d5f7
Eri Koyano <eri.koyano@gmail.com>
2021-07-07 11:22:20 +0900

    change main image

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Main.css";
-import coffeevid from "../../images/coffee2.mp4";
+// import coffeevid from "../../images/coffee2.mp4";
+import coffeeimage from "../../images/coffee3.jpg";
 import { Parallax } from "react-parallax";
 
 function Main() {
   return (
     <div className="video-container">
-      <video autoPlay muted loop className="coffeeimage">
+      {/* <video autoPlay muted loop className="coffeeimage">
         <source src={coffeevid} type="video/mp4" />
-      </video>
+      </video> */}
+      <img src={coffeeimage} className="coffeeimage" alt="coffe" />
     </div>
   );
 }
